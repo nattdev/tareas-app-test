264940abe30239ca2f4709937e44d44fe240d1c3
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-01-02 14:39:24 -0500

    Clase 03 Completada

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ const input = document.querySelector("[data-form-input]");
 const createTask = (e) => {
     e.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    console.log(value);
+    input.value = "";
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    task.innerHTML = content;
+    list.appendChild(task);
 }
 
 btn.addEventListener("click", createTask)
